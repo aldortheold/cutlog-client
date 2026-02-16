e88214b754f6c52fbb3ae69d1bc3364d81737c24
Andrew <aldortheold@gmail.com>
2026-02-16 22:35:25 +1000

    styled /targets

--- src/Targets.jsx
+++ src/Targets.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export default function Targets({ page, setPage }) {
 
-    const [form, setForm] = useState({ calories: "", protein: "", fat: "", addedSugar: "", water: "" });
+    const [form, setForm] = useState({ calories: "0", protein: "0", fat: "0", addedSugar: "0", water: "0" });
 
     useEffect(() => {
         setPage("/targets");
@@ -33,20 +33,28 @@ export default function Targets({ page, setPage }) {
 
     return (
         <motion.div
-            className='manage-presets'
+            className='targets'
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             exit={{opacity: 0}}
             transition={{duration: 0.5}}
         >
-            <h2>Manage Presets</h2>
+            <h2>Edit Targets & Limits</h2>
             <form onSubmit={handleSubmit}>
-                <input type="number" placeholder="Calories" name="calories" value={form.calories} onChange={handleChange} />
-                <input type="number" placeholder="Protein" name="protein" value={form.protein} onChange={handleChange} />
-                <input type="number" placeholder="Fat" name="fat" value={form.fat} onChange={handleChange} />
-                <input type="number" placeholder="AddedSugar" name="addedSugar" value={form.addedSugar} onChange={handleChange} />
-                <input type="number" placeholder="Water" name="water" value={form.water} onChange={handleChange} />
-                <button type="submit">Save</button>
+                <label htmlFor="calories">Calories</label>
+                <input type="number" placeholder="Calories" id="calories" name="calories" value={form.calories} onChange={handleChange} />
+                <label htmlFor="protein">Protein</label>
+                <input type="number" placeholder="Protein" id="protein" name="protein" value={form.protein} onChange={handleChange} />
+                <label htmlFor="fat">Fat</label>
+                <input type="number" placeholder="Fat" id="fat" name="fat" value={form.fat} onChange={handleChange} />
+                <label htmlFor="addedSugar">Added sugar</label>
+                <input type="number" placeholder="AddedSugar" id="addedSugar" name="addedSugar" value={form.addedSugar} onChange={handleChange} />
+                <label htmlFor="water">Water</label>
+                <input type="number" placeholder="Water" id="water" name="water" value={form.water} onChange={handleChange} />
+                <button type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                    Save
+                </button>
             </form>
         </motion.div>
     );
